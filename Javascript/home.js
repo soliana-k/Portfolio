59017abf563a28829d07ca24d858bbc64989a803
soliana-k <kalukassahun29@gmail.com>
2023-05-24 11:51:34 -0700

    portfolio

--- Javascript/home.js
+++ Javascript/home.js
@@ -1,1 +1,23 @@
+var drop=document.getElementById("drop");
+function haber(){
+  
+  var na=document.getElementById("men-bar");
+  if(na.style.display==="none"){
+    na.style.display="block";
+  }
+  else{
+    na.style.display="none";
+  }
+  
+  
+}
 
+function cod(){
+  var cd= document.getElementById("sid");
+  if(cd.style.display==="none"){
+    cd.style.display="flex";
+  }
+  else{
+    cd.style.display="none";
+  }
+}
